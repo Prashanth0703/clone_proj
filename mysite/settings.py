b6e28610efa08015f4cae7ce994c6b86bcd93d89
Prashanthp0703 <prashanthp0703@gmail.com>
2020-07-15 13:41:20 +0530

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # print(STATIC_ROOT)
 
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
